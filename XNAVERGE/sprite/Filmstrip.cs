a38c0b69f1e765cbacdeb3377059c237bdfed254
Jeff Hutchins <zumpiez@gmail.com>
2012-03-30 23:12:51 -0700

    Filmstrip animations

---
+++ XNAVERGE/sprite/Filmstrip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace XNAVERGE
+{
+    public class Filmstrip
+    {
+        Rectangle frameSize;
+        IList<int> frames;
+        TimeSpan frameDuration;
+        DateTime startTime;
+        
+        public Filmstrip(Rectangle frameSize, IList<int> frames, TimeSpan frameDuration)
+        {
+            this.frameSize = frameSize;
+            this.frames = frames;
+            this.frameDuration = frameDuration;
+            this.startTime = DateTime.Now;
+        }
+
+        public Rectangle ProcessAnimation()
+        {
+            TimeSpan timeSinceAnimationStarted = DateTime.Now - startTime;
+            int animationIndex = (int)(timeSinceAnimationStarted.TotalMilliseconds / frameDuration.TotalMilliseconds);
+            animationIndex %= frames.Count; //loop animation
+
+            Rectangle result = frameSize;
+            result.X = frameSize.Width * animationIndex;
+
+            return result;
+        }
+    }
+}
