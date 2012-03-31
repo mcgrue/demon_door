05046d50aefea1278094721f4d68fe875bf992c0
Colin Bayer <vogon@icculus.org>
2012-03-30 21:36:59 -0700

    hello, physics

--- DemonDoor/DemonDoor/Game1.cs
+++ DemonDoor/DemonDoor/Game1.cs
@@ -10,6 +10,9 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
 using Box2DX;
+using Box2DX.Collision;
+using Box2DX.Common;
+using Box2DX.Dynamics;
 
 namespace DemonDoor {
     /// <summary>
@@ -24,6 +27,9 @@ namespace DemonDoor {
             Content.RootDirectory = "Content";
         }
 
+        private World _world;
+        private Body _ground, _box;
+
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
@@ -32,6 +38,38 @@ namespace DemonDoor {
         /// </summary>
         protected override void Initialize() {
             // TODO: Add your initialization logic here
+            Vec2 gravity = new Vec2(0.0f, -10.0f);
+
+            // set up the world
+            AABB worldAabb = new AABB();
+            worldAabb.LowerBound = new Vec2(-10000, -10000);
+            worldAabb.UpperBound = new Vec2(10000, 10000);
+
+            _world = new World(worldAabb, gravity, true);
+
+            // set up the ground
+            BodyDef groundDef = new BodyDef();
+            groundDef.Position.Set(0f, -10f);
+
+            _ground = _world.CreateBody(groundDef);
+
+            PolygonDef boxDef = new PolygonDef();
+            boxDef.SetAsBox(50f, 10f);
+
+            _ground.CreateShape(boxDef);
+            _ground.SetMass(new MassData { Mass = 0.0f });
+
+            // set up the ball
+            BodyDef ballDef = new BodyDef();
+            ballDef.Position.Set(0f, 100f);
+
+            _box = _world.CreateBody(ballDef);
+
+            CircleDef circleDef = new CircleDef();
+            circleDef.Radius = 2f;
+
+            _box.CreateShape(circleDef);
+            _box.SetMass(new MassData { Mass = 1.0f });
             
             base.Initialize();
         }
@@ -65,6 +103,12 @@ namespace DemonDoor {
             if( GamePad.GetState( PlayerIndex.One ).Buttons.Back == ButtonState.Pressed )
                 this.Exit();
 
+            _world.Step(1f / 60f, 6, 2);
+            Vec2 ballPos = _box.GetPosition();
+            float ballAngle = _box.GetAngle();
+
+            Console.Out.WriteLine("({0}, {1}), {2}", ballPos.X, ballPos.Y, ballAngle);
+
             // TODO: Add your update logic here
 
             base.Update( gameTime );
@@ -75,7 +119,7 @@ namespace DemonDoor {
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw( GameTime gameTime ) {
-            GraphicsDevice.Clear( Color.LimeGreen );
+            GraphicsDevice.Clear( Microsoft.Xna.Framework.Color.LimeGreen );
 
             // TODO: Add your drawing code here
 
