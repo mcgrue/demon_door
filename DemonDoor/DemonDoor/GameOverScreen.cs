c6865f58a77c0379255847bb17ec07de610d6be7
Colin Bayer <vogon@icculus.org>
2012-03-31 21:53:33 -0700

    added evil window, game over screen

---
+++ DemonDoor/DemonDoor/GameOverScreen.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using XNAVERGE;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DemonDoor
+{
+    class GameOverScreen : Screen
+    {
+        public McGrenderStack mcg;
+        internal override void Load()
+        {
+            Game1 game1 = (Game1)Game1.game;
+            //Vector2[] verts = new [] {
+            //    new Vector2 { X = -100, Y = 0 },
+            //    new Vector2 { X = -70, Y = 0 },
+            //    new Vector2 { X = -100, Y = 30 }
+            //};
+
+            //Wall _wallTri = new Wall(_world, verts);
+            //McgNode rendernode;
+
+            mcg = new McGrenderStack();
+            Game1.game.setMcGrender(mcg);
+
+            mcg.AddLayer("background");
+
+            //McgLayer l = mcg.GetLayer("background");
+            ///// this is wrong.
+            //Rectangle rectTitle = new Rectangle(0, 0, 320, 240);
+            //rendernode = l.AddNode(
+            //    new McgNode(game1.im_title, rectTitle, l, 0, 0)
+            //);
+
+        }
+
+        internal override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Space))
+            {
+                Game1 game1 = Game1.game as Game1;
+
+                game1.LoadLevel("title");
+            }
+        }
+
+        internal override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch batch, Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            Game1 game1 = (Game1)Game1.game;
+            string doorSpeedDesc = string.Format("game over");
+            Vector2 size = game1.ft_hud24.MeasureString(doorSpeedDesc);
+
+            batch.DrawString(game1.ft_hud24, doorSpeedDesc, new Vector2 { X = (640 - size.X) / 2, Y = 240 }, Color.White);
+        }
+
+    }
+}
---
+++ DemonDoor/DemonDoor/WindowSprite.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using XNAVERGE;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace DemonDoor
+{
+    class WindowSprite
+    {
+        public Texture2D texture { get; set; }
+        private Filmstrip currentAnimation;
+
+        public Sprite Sprite { get; private set; }
+
+        public enum AnimationState
+        {
+            Idle
+        }
+
+        public WindowSprite(SpriteBasis sb)
+        {
+            animationAtlas = new Dictionary<AnimationState, Filmstrip>();
+            animationAtlas[AnimationState.Idle] = createFilmstrip(0);
+
+            this.currentAnimation = animationAtlas[AnimationState.Idle];
+            this.Sprite = new Sprite(sb, currentAnimation);
+        }
+
+        public void SetAnimationState(AnimationState state)
+        {
+            this.currentAnimation = animationAtlas[state];
+            Sprite.set_animation(currentAnimation);
+        }
+
+        /// <summary>
+        /// Creates a filmstrip with a single animation frame
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <returns></returns>
+        private Filmstrip createFilmstrip(int frame)
+        {
+            return createFilmstrip(new[] { frame });
+        }
+        /// <summary>
+        /// Creates a filmstrip with a list of animation frames
+        /// </summary>
+        /// <param name="frames"></param>
+        /// <returns></returns>
+        private Filmstrip createFilmstrip(IList<int> frames,  bool randomizedStartFrame = false)
+        {
+            return new Filmstrip(new Point(20, 17), frames, 150, randomizedStartFrame);
+        }
+
+        private Dictionary<AnimationState, Filmstrip> animationAtlas;
+    }
+}
